5fbf6a1c25319ce448792fce044b2bbb0abd5844
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2022-11-24 21:27:52 -0300

    salvar

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function MaiorNumero(){
+let n1 = document.getElementById("numero1").value
+let n2 = document.getElementById("numero2").value
+if (parseInt(n1) > parseInt(n2)) {
+    document.getElementById("resultado").innerHTML = "o maior número é:" + n1
+
+} else {
+    document.getElementById("resultado").innerHTML = "o maior número é:" + n2
+}
+
+}
