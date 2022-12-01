e3bc232bf5431a5444025d991fc4169c015b8fb3
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2022-12-01 19:28:16 -0300

    salvo1

--- ex005/app.js
+++ ex005/app.js
@@ -2,12 +2,19 @@
 // O código deve pedir 3 informações: valor do empréstimo, número de parcelas e salário do solicitante.
 // Aprovar empréstimo caso o valor das parcelas representem no máximo 30% do salário do solicitante.
 
-Function aprovaremprestimo(){
+function aprovaremprestimo() {
 
 
-let emprestimo = document.getElementById('numero1')
-let emprestimo = document.getElementById('numero2')
-let emprestimo = document.getElementById('numero3')
+    let valoremprestimo = document.getElementById('valoremprestimo')
+    let numeroparcelas = document.getElementById('numeroparcelas')
+    let salario = document.getElementById('salario')
 
+    let trintaporcento = parseFloat(salario) * 0.30;
+    let valorparcelas = valoremprestimo / numeroparcelas
 
+    if(valorparcelas <=trintaporcento){
+        aprovado
+    }else {
+        emprestio
+    }
 }
